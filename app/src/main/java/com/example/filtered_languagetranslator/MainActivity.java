b1b9b7a5616256443dbbe2af8978dc8499d059d5
janodin <japatundog@usm.edu.ph>
2024-08-15 22:13:55 +0800

    first commit

---
+++ app/src/main/java/com/example/filtered_languagetranslator/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.filtered_languagetranslator;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true); // Enable JavaScript
+
+        webView.setWebViewClient(new WebViewClient()); // Ensure links open within the app
+        webView.loadUrl("https://filtered.onrender.com"); // Replace with your website URL
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
